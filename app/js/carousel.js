7808047a8a2f8885f8060bcff6a64f744251d111
Tim Douglas <neurotech@gmail.com>
2016-06-14 16:01:35 +1000

    Refactoring directory structure

---
+++ app/js/carousel.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const page = require('page');
+var content = document.querySelector('#content');
+
+page.base('/picnic');
+page('*', function (ctx, next) {
+  if (ctx.init) {
+    next();
+  } else {
+    content.classList.add('transition');
+    setTimeout(function () {
+      content.classList.remove('transition');
+      next();
+    }, 225);
+  }
+});
+
+page('/splash', splash);
+page('/staff-absent', staffAbsent);
+page('/summary', summary);
+page();
+
+function splash () {
+  app.currentView = 'splash';
+}
+
+function staffAbsent () {
+  app.currentView = 'staff-absent';
+}
+
+function summary () {
+  app.currentView = 'summary';
+}
+
+setTimeout(function () {
+  page('/summary');
+}, 1100);
+
+// var carousel = ['/splash', '/staff-absent'];
+// var duration = 5000;
+//
+// function runCarousel (items) {
+//   var i = 1;
+//   setInterval(function () {
+//     page(items[i]);
+//     i++;
+//     if (i >= carousel.length) { i = 0; }
+//   }, duration);
+// }
+//
+// runCarousel(carousel);
+
+// setInterval(function () {
+//   page('/staff-absent');
+// }, 3200);
+//
+// setInterval(function () {
+//   page('/splash');
+// }, 5000);
--- app/js/nice-time.js
+++ app/js/nice-time.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const moment = require('moment');
-const config = require('./config');
+const config = require('../config');
 
 module.exports = function (time) {
   var niceTime = moment(time).format(config.timeStamps.format);
