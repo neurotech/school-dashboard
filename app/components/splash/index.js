15f9ec1208390d6ff729f0950c2104d5a62442f5
Tim Douglas <neurotech@gmail.com>
2016-06-16 23:42:13 +1000

    Simplified splash component

--- app/components/splash/index.js
+++ app/components/splash/index.js
@@ -2,15 +2,13 @@
 
 const path = require('path');
 const pug = require('pug');
-const config = require('../../config');
 
 module.exports = {
   template: pug.renderFile(path.join(__dirname, 'splash.pug')),
   data: function () {
     return {
-      title: '',
-      version: process.env.npm_package_version,
-      endpoint: config.get('api')
+      title: 'Picnic',
+      version: process.env.npm_package_version
     };
   }
 };
