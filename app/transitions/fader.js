b435eaf7e7255d0a78a7968b0cfe34b558c055c4
Tim Douglas <neurotech@gmail.com>
2016-06-20 15:20:19 +1000

    Modularised fader transition

---
+++ app/transitions/fader.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Velocity = require('velocity-animate');
+
+module.exports = {
+  type: 'animation',
+  css: false,
+  enter: function (el) {
+    el.style.opacity = 0;
+    Velocity(el, { opacity: 1 }, 1000);
+  },
+  leave: function (el) {
+    Velocity(el, { opacity: 0 }, 1000);
+  }
+};
