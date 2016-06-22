ad956179262fd75b4609e5aef2ccb039e3453304
Tim Douglas <neurotech@gmail.com>
2016-06-23 08:46:29 +1000

    Added pageChange transition to splash

--- app/components/splash/index.js
+++ app/components/splash/index.js
@@ -2,13 +2,18 @@
 
 const path = require('path');
 const pug = require('pug');
+const pageChange = require('../../transitions/page-change');
 
 module.exports = {
   template: pug.renderFile(path.join(__dirname, 'splash.pug')),
   data: function () {
     return {
       title: 'Picnic',
+      description: 'Passive Information Communicator for New and Interesting Content',
       version: process.env.npm_package_version
     };
+  },
+  activate: function (done) {
+    pageChange(done);
   }
 };
