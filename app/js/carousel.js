5226a678cad58248642f43a8205c0b12e4f8164f
Tim Douglas <neurotech@gmail.com>
2016-06-23 08:42:50 +1000

    Refactored carousel to not use page

--- app/js/carousel.js
+++ app/js/carousel.js
@@ -1,48 +1,16 @@
 'use strict';
 
-const page = require('page');
-var content = document.querySelector('#content');
 var carousel = {};
-
-page.base('/picnic');
-page('*', function (ctx, next) {
-  if (ctx.init) {
-    next();
-  } else {
-    content.classList.add('transition');
-    setTimeout(function () {
-      content.classList.remove('transition');
-      next();
-    }, 225);
-  }
-});
-
-page('/splash', splash);
-page('/staff-absent', staffAbsent);
-page('/summary', summary);
-page();
-
-function splash () {
-  app.currentView = 'splash';
-}
-
-function staffAbsent () {
-  app.currentView = 'staff-absent';
-}
-
-function summary () {
-  app.currentView = 'summary';
-}
 
 carousel.start = function (items, duration) {
   if (items.length === 1) {
     setTimeout(function () {
-      page(items[0]);
+      app.currentView = items[0];
     }, duration);
   } else {
     var i = 0;
     setInterval(function () {
-      page(items[i]);
+      app.currentView = items[i];
       i++;
       if (i >= items.length) { i = 0; }
     }, duration);
