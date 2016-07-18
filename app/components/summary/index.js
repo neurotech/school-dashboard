25c8964325aa3cbf59481bebb62296112a78ffa6
Tim Douglas <neurotech@gmail.com>
2016-07-18 15:33:09 +1000

    Experimenting with summary component

--- app/components/summary/index.js
+++ app/components/summary/index.js
@@ -2,16 +2,19 @@
 
 const path = require('path');
 const pug = require('pug');
+const pageChange = require('../../transitions/page-change');
 
 module.exports = {
   template: pug.renderFile(path.join(__dirname, 'summary.pug')),
   data: function () {
     return {
-      barChartData: [12, 19, 3]
+      stat: 19
     };
   },
+  activate: function (done) {
+    pageChange(done);
+  },
   components: {
-    'bar-chart': require('../bar-chart'),
-    'pie-chart': require('../pie-chart')
+    'counter': require('../counter')
   }
 };
